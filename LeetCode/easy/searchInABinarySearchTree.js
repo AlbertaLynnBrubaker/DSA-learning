4335acbf59c5108398adb57aba31eb6de934ae93
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-01-19 11:51:25 -0800

    searchInABinarySearchTree

---
+++ LeetCode/easy/searchInABinarySearchTree.js
@@ -0,0 +1,10 @@
+const { c } = require('../helper/consoleLogHelper')
+
+var searchBST = function(root, val) {
+  if(!root) return null
+
+  if(root.val === val) return root  
+  
+  return searchBST(root.left, val) || searchBST(root.right, val)
+}
+
