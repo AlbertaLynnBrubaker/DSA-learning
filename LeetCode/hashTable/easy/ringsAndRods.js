b42d0072753f8f920649cea8f2dadef29069d347
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-01-26 14:00:35 -0800

    ringsAndRods

--- LeetCode/hashTable/easy/ringsAndRods.js
+++ LeetCode/hashTable/easy/ringsAndRods.js
@@ -1,5 +1,26 @@
-const { c } = require('../../helper/consoleLogHelper')
+
+
+const rings = "B0R0G0R9R0B0G0"
 
 var countPoints = function(rings) {
-    
+  const map = new Map()
+  let result = 0
+
+  for(let i = 0; i < rings.length; i += 2 ) {
+    let color = rings[i]
+    let key = `${rings[i + 1]}`
+    if(!map.has(key)) {
+      let set = new Set()
+      map.set(key, set.add(color))
+    } else {
+      map.get(key).add(color)
+    }
+  }
+
+  for(let ring of map.values()) {
+    if(ring.has("B") && ring.has("G") && ring.has("R")) result++
+  }
+  return result
 };
+
+c({res: countPoints(rings)})
