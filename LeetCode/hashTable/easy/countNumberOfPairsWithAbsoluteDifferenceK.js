4846a5a244d0958d8f7e516f078f5b24679606d8
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-01-26 11:55:03 -0800

    countNumberDiffAbsK countNumberOfConsistentStrings

--- LeetCode/hashTable/easy/countNumberOfPairsWithAbsoluteDifferenceK.js
+++ LeetCode/hashTable/easy/countNumberOfPairsWithAbsoluteDifferenceK.js
@@ -1,5 +1,26 @@
-const { c } = require('../../helper/consoleLogHelper')
+
+
+const nums1 = [3,2,1,5,4]
 
 var countKDifference = function(nums, k) {
-    
+  const map = {}
+  let count = 0
+
+  for(let num of nums) {
+    map[num] = map[num] ? map[num] + 1 : 1
+  }
+
+  while(nums.length) {
+    let key = nums.pop()
+    map[key]--
+    if(map[key] === 0) delete map[key]
+    let plus = key + k
+    let minus = key - k
+    if(map[plus]) count += map[plus]
+    if(map[minus]) count += map[minus]
+  }
+
+  return count
 };
+
+c({res: countKDifference(nums1, 2)})
