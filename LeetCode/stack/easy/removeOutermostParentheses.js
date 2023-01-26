6b60bd4ffae245594997b9053868d1608fb160eb
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-01-26 15:13:25 -0800

    removeOuterParentheses

--- LeetCode/stack/easy/removeOutermostParentheses.js
+++ LeetCode/stack/easy/removeOutermostParentheses.js
@@ -1,1 +1,25 @@
-const { c } = require('../../helper/consoleLogHelper')
+
+
+var removeOuterParentheses = function(s) {
+  let stack = 0
+  let results = "" 
+
+  for(let l of s) {
+    if(l === "(" && stack === 0) {
+      stack++
+    } else if(l === "(") {
+      results += l
+      stack++
+    } 
+
+    if(l === ")" && stack === 1) {
+      stack--
+    } else if(l === ")") {
+      results += l
+      stack--
+    } 
+  }
+  return results
+};
+
+c({res: removeOuterParentheses("(()())(())(()(()))")})
