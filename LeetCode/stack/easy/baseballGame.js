32762d10cfc6874484412490a300d404f7defe58
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-01-26 16:41:31 -0800

    baseballGame

--- LeetCode/stack/easy/baseballGame.js
+++ LeetCode/stack/easy/baseballGame.js
@@ -1,5 +1,34 @@
-const { c } = require('../../helper/consoleLogHelper')
+
 
 var calPoints = function(operations) {
-    
+  const record = []
+  let score = 0
+
+  operations.forEach(op => {
+    let temp = 0
+    switch (op) {
+      case "C":
+        score -= record.pop()
+        return
+      case "D":
+        temp = (record[record.length -1] * 2)
+        record.push(temp)
+        score += temp
+        return
+      case "+":
+        temp = record[record.length -1] + record[record.length - 2]
+        record.push(temp)
+        score += temp
+        return
+      default:
+        temp = parseInt(op)
+        record.push(temp)
+        score += temp
+        return
+    }
+  })
+
+  return score
 };
+
+c({res: calPoints(["5","-2","4","C","D","9","+","+"])})
---
+++ LeetCode/stack/easy/nextGreaterElementI.js
@@ -0,0 +1,5 @@
+const { c } = require('../../helper/consoleLogHelper')
+
+var nextGreaterElement = function(nums1, nums2) {
+    
+};
