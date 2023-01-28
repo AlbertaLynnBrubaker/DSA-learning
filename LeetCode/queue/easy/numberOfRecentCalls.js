5861876a611a153e4c28b89af295180d8bf7f7b1
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-01-27 16:15:27 -0800

    queue folder numberOfRecentCalls

---
+++ LeetCode/queue/easy/numberOfRecentCalls.js
@@ -0,0 +1,52 @@
+const { c } = require('../../helper/consoleLogHelper')
+
+class RecentCounter {
+  constructor() {
+    this.requests = {
+      head: null,
+      tail: null,
+      length: 0
+    }
+    this.range = []
+  }
+
+  ping(time) {
+    const newTime = {
+      time: time,
+      next: null
+    }
+    if(!this.requests.length) {
+      this.requests.head = newTime
+      this.requests.tail = newTime
+      this.requests.length++
+    } else {
+      this.requests.tail.next = newTime
+      this.requests.tail = this.requests.tail.next
+      this.requests.length++
+    }
+    this.range = [time - 3000, time]
+    while(this.requests.head.time < this.range[0]) {
+      this.requests.head = this.requests.head.next
+      this.requests.length--
+    }
+    return this.requests.length
+  }
+}
+
+const rC = new RecentCounter()
+
+c({res: rC.ping(1)})
+c({res: rC.ping(100)})
+c({res: rC.ping(201)})
+c({res: rC.ping(3300)})
+// var RecentCounter = function() {
+    
+// };
+
+// /** 
+//  * @param {number} t
+//  * @return {number}
+//  */
+// RecentCounter.prototype.ping = function(t) {
+    
+// };
--- LeetCode/stack/easy/makeTheStringGreat.js
+++ LeetCode/stack/easy/makeTheStringGreat.js
@@ -1,7 +1,23 @@
-const { c } = require('../../helper/consoleLogHelper')
+
 
 var makeGood = function(s) {
-    
+  const stack = []
+
+  for(let current of s) {
+    if(!stack.length) stack.push(current)
+    else {
+      let stackLast = stack[stack.length - 1]
+      if(isBadPair(stackLast, current)) stack.pop()
+      else stack.push(current)
+    }
+  }
+
+  return stack.join('')  
 };
 
-c({res: makeGood()})
+const isBadPair = function(char1, char2) {
+  let difference = char1.charCodeAt(0) - char2.charCodeAt(0)
+  return Math.abs(difference) === 32
+}
+
+c({res: makeGood("leEeetcode")})
