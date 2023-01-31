e593db874debc26c2c490642b65ab9f38d069c27
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-01-30 18:04:30 -0800

    reverseWordsInAStringIII

--- LeetCode/queue/easy/implementStackUsingQueues.js
+++ LeetCode/queue/easy/implementStackUsingQueues.js
@@ -1,4 +1,4 @@
-const { c } = require('../../helper/consoleLogHelper')
+
 
 // FUCKING POINTLESS LEETCODE PROBLEM
 
---
+++ LeetCode/twoPointers/easy/reverseWordsInAStringIII.js
@@ -0,0 +1,24 @@
+const { c } = require('../../helper/consoleLogHelper')
+
+var reverseWords = function(s) {
+  const str = []
+  let left = 0
+  let right = 0
+  while(left < s.length - 1) {
+    if(s[right] !== " " && right < s.length) {
+      right++
+      continue
+    }
+    
+    for(let i = right - 1; i >= left; i--) {
+      str.push(s[i])
+    }
+
+    if(right < s.length) str.push(" ")
+    right++
+    left = right
+  }
+  return str.join("")
+};
+
+c({res: reverseWords("Let's take LeetCode contest")})
