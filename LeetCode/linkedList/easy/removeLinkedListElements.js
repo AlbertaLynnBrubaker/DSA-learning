ef88fa991ff4484c2bcdc57f93aee147ea6c8a44
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-02-08 12:21:23 -0800

    linkedListCycle

---
+++ LeetCode/linkedList/easy/removeLinkedListElements.js
@@ -0,0 +1,6 @@
+const { c } = require('../../helper/consoleLogHelper')
+const { createList } = require('../../helper/createLinkedListFromArray')
+
+var removeElements = function(head, val) {
+  
+};
