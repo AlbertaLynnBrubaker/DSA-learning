d8f4810e7223bd9c0efe1ac9f0951d2900a59bd4
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-02-07 17:13:42 -0800

    moveZeroes

--- LeetCode/twoPointers/easy/moveZeroes.js
+++ LeetCode/twoPointers/easy/moveZeroes.js
@@ -1,5 +1,24 @@
 const { c } = require('../../helper/consoleLogHelper')
 
 var moveZeroes = function(nums) {
-  
+  let left = 0
+  let right = left + 1
+
+  while(right < nums.length) {
+    if(nums[left] !== 0 && nums[right] !== 0) {
+      left++
+      right++
+    } else if(nums[left] === 0 && nums[right] === 0) {
+      right++
+    } else if(nums[right] === 0) {
+      left++
+    } else {
+      let temp = nums[left]
+      nums[left] = nums[right]
+      nums[right] = temp
+      left++
+    }
+  }
 };
+
+c({res: moveZeroes([10,1,0,3,12])})
