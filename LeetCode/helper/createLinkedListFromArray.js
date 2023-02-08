8b425fec46abc45ea1b0120cdc75ae8839dbff60
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-02-08 12:46:46 -0800

    removeLinkedListElements

--- LeetCode/helper/createLinkedListFromArray.js
+++ LeetCode/helper/createLinkedListFromArray.js
@@ -1,49 +1,104 @@
-class LinkedList {
-  constructor() {
-    this.head = null
-    this.tail = null
-    this.length = 0
-  }
-
-  insert(val) {
-    const node = new Node(val)
-    if(!this.head) {
-      this.head = node
-      this.tail = this.head
-      this.length++
-    } else {
-      this.tail.next = node
-      this.tail = this.tail.next
-      this.length++
-    }
-    return this
-  }
-
-  remove() {
-    if(!this.head) return null
-    if(!this.head.next) {
-      this.head = null
-      this.tail = null
-      this.length--
-    } else {
-      this.head = this.head.next
-      this.length--
-    }
-    return this
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
   }
 }
 
-class Node {
-  constructor(val) {
-    this.val = val
-    this.next = null
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    },
+    this.tail = this.head
+    this.length = 1
   }
+
+  append(value) {
+    const newNode = new Node(value)
+
+    this.tail.next = newNode
+    this.tail = newNode
+    this.length++
+    return this
+  }
+
+  prepend(value) {
+    const newNode = new Node(value)
+
+    newNode.next = this.head
+    this.head = newNode
+    this.length++
+    return this
+  }
+
+  printList() {
+    const array = []
+    let currentNode = this.head
+    while(currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    console.log( array )
+  }
+
+  insert(index, value) {
+    //check inputs    
+    if(index === 0) {
+      this.prepend(value)
+      return this.printList()
+    }
+    if(index >= this.length) {
+      this.append(value)
+      return this.printList()
+    }
+    const newNode = new Node(value)
+
+    const leader = this.traverseToIndex(index - 1) 
+    const holdingPointer = leader.next
+    leader.next = newNode
+    newNode.next = holdingPointer
+    this.length++
+    return this.printList()
+  }
+
+  remove(index) {
+    if(index < 0 || index >= this.length) {
+      return console.log("index does not exist")
+    }
+    if(index === 0) {
+      this.head = this.head.next
+      this.length--
+      return this.printList()
+    }
+
+    const leader = this.traverseToIndex(index - 1)
+    const nodeAtIndex = leader.next
+    const nextNode = nodeAtIndex.next
+    leader.next = nextNode
+    if(index === this.length - 1) {      
+      this.tail = leader
+    }
+    this.length--
+    return this.printList()
+  }
+
+  traverseToIndex(index) {
+    let counter = 0
+    let currentNode = this.head
+    while (counter !== index) {
+      currentNode = currentNode.next
+      counter++
+    }
+    return currentNode
+  }  
 }
 
 const createList = function(array) {
   const list = new LinkedList()
   for(ele of array) {
-    list.insert(ele)
+    list.append(ele)
   }
   return list
 }
---
+++ LeetCode/linkedList/medium/designBrowserHistory.js
@@ -0,0 +1,3 @@
+const { c } = require('../../helper/consoleLogHelper')
+const { createList } = require('../../helper/createLinkedListFromArray')
+
