fc0d4e5ab92e502f579b53a5a0a7c0e386307eb6
Alberta Lynn Brubaker <findingalberta@gmail.com>
2023-02-10 13:03:42 -0800

    sortingTheSentence

--- LeetCode/sorting/easy/sortingTheSentence.js
+++ LeetCode/sorting/easy/sortingTheSentence.js
@@ -1,5 +1,14 @@
 const { c } = require('../../helper/consoleLogHelper')
 
 var sortSentence = function(s) {
-  
+  s = s.split(' ')
+
+  let results = new Array(s.length)
+  for(let i = 0; i < s.length; i++) {
+    let index = s[i][s[i].length - 1] - 1
+    results[index] = s[i].slice(0, -1)
+  }
+  return results.join(' ')
 };
+
+c({res: sortSentence("sentence4 a3 is2 This1")})
